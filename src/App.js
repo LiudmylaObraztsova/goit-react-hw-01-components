e3f97152c213875c92bd36778ff32dffc6b3f17b
LiudmylaObraztsova <lpfoneone@gmail.com>
2021-10-07 01:49:29 +0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-
+import React from 'react';
+import Profile from './components/Profile';
+import user from './user.json'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    REACT-HW-01
-    </div>
-  );
-}
+  return <div>
+    
+    <Profile
+      src={user.avatar}
+      alt={user.name}
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      followers={user.stats.followers}
+      views={user.stats.views}
+      likes={user.stats.likes} />
+  
 
+  </div>
+}
 export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,43 @@
+import defaultImage from '../nophoto.jpg'
+
+export default function Profile({ src=defaultImage, alt, name, tag, location, followers, views, likes }) {
+
+  return <div class="profile">
+  <div class="description">
+    <img
+      src={src}
+      alt={alt}
+      class="avatar"
+    />
+      <p class="name">{name }</p>
+      <p class="tag">{ tag}</p>
+      <p class="location">{ location}</p>
+  </div>
+
+  <ul class="stats">
+    <li>
+      <span class="label">Followers: </span>
+        <span class="quantity">{ followers}</span>
+    </li>
+    <li>
+      <span class="label">Views: </span>
+      <span class="quantity">{views}</span>
+    </li>
+    <li>
+      <span class="label">Likes: </span>
+      <span class="quantity">{likes}</span>
+    </li>
+  </ul>
+</div>
+}
+
+
+// ReactDOM.render(<Profile
+//   src={user.avatar}
+//   alt={user.name}
+//   name={user.name}
+//   tag={user.tag}
+//   location={user.location}
+//   followers={user.stats.followers}
+//   views={user.stats.views}
+//   likes={user.stats.likes} />, document.querySelector('#root'))
